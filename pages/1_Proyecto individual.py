2be8118d6a267dcfbdd8964dfce09c11540b309e
fauserics <patriciocarrilero@gmail.com>
2025-11-04 16:17:48 -0300

    Add Streamlit page for individual project

---
+++ pages/1_Proyecto individual.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from greenscore_core import page_proyecto_individual
+
+st.set_page_config(page_title="Proyecto individual – GreenScore", page_icon="🌿", layout="wide")
+page_proyecto_individual()
