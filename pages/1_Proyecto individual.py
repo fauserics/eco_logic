173adcdb3345c67a8bcd095b1412b271dc0f04d6
fauserics <patriciocarrilero@gmail.com>
2025-11-24 17:42:36 -0300

    Add language selector to Proyecto individual page

--- pages/1_Proyecto individual.py
+++ pages/1_Proyecto individual.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from greenscore_core import page_proyecto_individual
+from greenscore_core import page_proyecto_individual, language_selector, _t
+
+language_selector()
 
 st.set_page_config(page_title="Proyecto individual – GreenScore", page_icon="🌿", layout="wide")
 page_proyecto_individual()
