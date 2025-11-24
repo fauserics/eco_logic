d9f8daa6a59157bf46f5de077f68da8e4b983675
fauserics <patriciocarrilero@gmail.com>
2025-11-24 16:05:21 -0300

    Update caption text in Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -41,4 +41,4 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.caption("© EcoLogic – GreenScore · Demo con módulo ISO 50001, reporte LLM y exportación PDF.")
+st.caption("© GreenScore - AInergy Score Audit · Demo con módulo ISO 50001, reporte LLM y exportación PDF.")
