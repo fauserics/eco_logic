78a8c5eb9d40eea693a0196068e46bfb0236e84e
fauserics <patriciocarrilero@gmail.com>
2025-11-24 17:47:39 -0300

    Add language selector to portfolio page

--- pages/2_Portfolio.py
+++ pages/2_Portfolio.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from greenscore_core import page_portfolio
+from greenscore_core import page_portfolio, language_selector
+
+language_selector()
 
 st.set_page_config(page_title="Portfolio – GreenScore", page_icon="🌿", layout="wide")
 page_portfolio()
