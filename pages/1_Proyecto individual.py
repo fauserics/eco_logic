38656a21a41e277461c2d813c6a0da7f609d6248
fauserics <patriciocarrilero@gmail.com>
2025-11-24 18:05:29 -0300

    Update 1_Proyecto individual.py

--- pages/1_Proyecto individual.py
+++ pages/1_Proyecto individual.py
@@ -1,7 +1,16 @@
 import streamlit as st
-from greenscore_core import page_proyecto_individual, language_selector
+from greenscore_core import language_selector, get_lang, page_proyecto_individual
 
 st.set_page_config(page_title="Proyecto individual – GreenScore", page_icon="🌿", layout="wide")
 
-language_selector()
+# Si querés un título arriba de la subpágina:
+if lang == "en":
+    st.title("AInergy Score Audit")
+else:
+    st.title("AInergy Score Audit")
+
+
+
+# Selector de idioma en el sidebar
+lang = get_lang()
 page_proyecto_individual()
