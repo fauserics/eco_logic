fcb116507b8a977dcdb6fca4bf09fd74a01603df
fauserics <patriciocarrilero@gmail.com>
2025-11-24 17:48:25 -0300

    Reorder language_selector call in Portfolio page

--- pages/2_Portfolio.py
+++ pages/2_Portfolio.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from greenscore_core import page_portfolio, language_selector
 
-language_selector()
-
 st.set_page_config(page_title="Portfolio – GreenScore", page_icon="🌿", layout="wide")
+
+language_selector()
 page_portfolio()
