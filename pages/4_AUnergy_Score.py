c5c9fd002f5fb5540a656e8da5ca0e3fad349895
fauserics <patriciocarrilero@gmail.com>
2025-11-24 17:50:16 -0300

    Add language selector to AUnergy Score page

--- pages/4_AUnergy_Score.py
+++ pages/4_AUnergy_Score.py
@@ -1,4 +1,6 @@
 import streamlit as st
-from greenscore_core import page_energy_management
+from greenscore_core import page_energy_management, language_selector
+
+language_selector()
 
 page_energy_management()
