90e6a6ab86e0945e0cfd8e175aef5666cb38b315
fauserics <patriciocarrilero@gmail.com>
2025-11-24 17:49:40 -0300

    Add language selector to Metodología page

--- pages/3_Metodología.py
+++ pages/3_Metodología.py
@@ -1,5 +1,8 @@
 import streamlit as st
-from greenscore_core import page_metodologia
+from greenscore_core import page_metodologia, language_selector
+
 
 st.set_page_config(page_title="Metodología – GreenScore", page_icon="🌿", layout="wide")
+
+language_selector()
 page_metodologia()
